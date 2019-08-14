4102b8d65231fb43e12d17da49d942303d7aa994
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-08-14 19:44:38 +0900

    Some fix

--- src/lib/mbgl-weather-symbol-control/index.ts
+++ src/lib/mbgl-weather-symbol-control/index.ts
@@ -1,5 +1,6 @@
 import mapboxgl from "mapbox-gl";
-import unknown from "./images/unnknown.png";
+import symbols from "./symbols";
+
 import wind12 from "./images/wind-12.png";
 import wind11 from "./images/wind-11.png";
 import wind10 from "./images/wind-10.png";
@@ -30,12 +31,50 @@ const wind = {
   11: wind11,
   12: wind12
 };
+type WindPower = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+const windDir = {
+  0: "北",
+  1: "北北東",
+  2: "北東",
+  3: "東北東",
+  4: "東",
+  5: "東南東",
+  6: "南東",
+  7: "南南東",
+  8: "南",
+  9: "南南西",
+  10: "南西",
+  11: "西南西",
+  12: "西",
+  13: "西北西",
+  14: "北西",
+  15: "北北西"
+};
+type DirectionRank =
+  | 0
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 13
+  | 14
+  | 15;
 
 export class WeatherSymbolControl {
   private container: HTMLDivElement;
-  private addButton: HTMLButtonElement;
   private map?: mapboxgl.Map;
   private weathers: GeoJSON.FeatureCollection<GeoJSON.Point>;
+
+  private selectedWeather: keyof typeof symbols = "unknown";
 
   private currentCenter?: mapboxgl.Point;
   private currentMarker?: mapboxgl.Marker;
@@ -45,13 +84,25 @@ export class WeatherSymbolControl {
     this.container = document.createElement("div");
     this.container.className = "mapboxgl-ctrl mapboxgl-ctrl-group";
 
-    this.addButton = document.createElement("button");
-    this.addButton.className = "mapboxgl-ctrl-icon mapboxgl-ctrl-zoom-in";
-    this.addButton.setAttribute("type", "button");
-    this.addButton.setAttribute("title", "add weather point");
-    this.addButton.setAttribute("aria-label", "add weather point");
+    Object.keys(symbols).forEach((key: string) => {
+      const button = document.createElement("button");
+      button.className = "weather-select-button";
+      const { symbol, subscript } = symbols[key as keyof typeof symbols];
+      button.innerHTML = `<img src="${symbol}" class="weather-selector" />`;
+      button.innerHTML += subscript
+        ? `<span class="subscript">${subscript}</span>`
+        : "";
+      button.addEventListener("click", () => {
+        const selected = document.querySelector(
+          ".weather-select-button.selected"
+        );
+        selected && selected.classList.remove("selected");
+        button.classList.add("selected");
+        this.selectedWeather = key as keyof typeof symbols;
+      });
+      this.container.appendChild(button);
+    });
 
-    this.container.appendChild(this.addButton);
     this.weathers = {
       type: "FeatureCollection",
       features: []
@@ -60,8 +111,8 @@ export class WeatherSymbolControl {
 
   onAdd(map: mapboxgl.Map) {
     map.on("click", this.onMapClick(map));
-    map.on("mousemove", this.onMapMouseMove(map));
-    map.on("transform", () => console.log(1));
+    map.on("mousemove", this.onMapMouseMove);
+    // map.on("keydown", this.onMapKeyDown);
     return this.container;
   }
 
@@ -74,7 +125,7 @@ export class WeatherSymbolControl {
       this.currentCenter = e.point;
 
       const weatherElement = document.createElement("img");
-      weatherElement.setAttribute("src", unknown);
+      weatherElement.setAttribute("src", symbols[this.selectedWeather].symbol);
       weatherElement.setAttribute("class", "weather");
 
       const windElement = document.createElement("img");
@@ -98,38 +149,50 @@ export class WeatherSymbolControl {
     }
   };
 
-  private onMapMouseMove = (map: mapboxgl.Map) => (
-    e: mapboxgl.MapMouseEvent
-  ) => {
+  private onMapMouseMove = (e: mapboxgl.MapMouseEvent) => {
     if (this.currentCenter && this.currentMarker) {
       const currentCenter = e.point;
       const dx = currentCenter.x - this.currentCenter.x;
       const dy = currentCenter.y - this.currentCenter.y;
 
-      const direction =
-        (4 + Math.floor((16 * ((Math.atan2(dy, dx) * 180) / Math.PI)) / 360)) *
-        (360 / 16);
+      const directionRank =
+        4 + Math.floor((16 * ((Math.atan2(dy, dx) * 180) / Math.PI)) / 360);
+      const direction = directionRank * (360 / 16);
 
       const bilateralCoefficient = 20;
       const windPower = Math.floor(
         Math.min(12, (dx ** 2 + dy ** 2) ** 0.5 / bilateralCoefficient)
-      );
+      ) as WindPower;
 
       const windElement = this.currentMarker
         .getElement()
         .querySelector(".wind");
 
-      console.log(this.currentMarker.getElement());
+      // const windDirKey = (directionRank < 0
+      //   ? 16 + directionRank
+      //   : directionRank) as DirectionRank;
 
       if (windElement) {
-        // @ts-ignore
         windElement.setAttribute("src", wind[windPower]);
-        // @ts-ignore
-        windElement.style.transform =
+        (windElement as HTMLImageElement).style.transform =
           this.windMarkerTransformBaseString + `rotate(${direction}deg)`;
+        // this.container.innerHTML = `${windDir[windDirKey]}の風 風力${windPower} 天気は不明`;
       }
     }
   };
+
+  // private onMapKeyDown = (e: any) => {
+  //   alert(1);
+  //   if (this.currentCenter && this.currentMarker) {
+  //     const weatherElement = this.currentMarker
+  //       .getElement()
+  //       .querySelector(".weather");
+  //
+  //     if (weatherElement) {
+  //       weatherElement.setAttribute("src", symbols.snow.symbol);
+  //     }
+  //   }
+  // };
 
   onRemove() {
     this.container.parentNode &&
--- src/lib/mbgl-weather-symbol-control/symbols.js
+++ src/lib/mbgl-weather-symbol-control/symbols.js
@@ -1,86 +1,117 @@
+import unknown from "./images/unnknown.png";
+import snowstorm from "./images/snowstorm.png";
+import sandstorm from "./images/sandstom.png";
+import dusthaze from "./images/dusthaze.png";
+import haze from "./images/haze.png";
+import hail from "./images/hail.png";
+import hailstone from "./images/hailstone.png";
+import snow from "./images/snow.png";
+import thunder from "./images/thunder.png";
+import sleet from "./images/sleet.png";
+import fog from "./images/fog.png";
+import cloudy from "./images/cloudy.png";
+import sunny from "./images/sunny.png";
+import clear from "./images/clear.png";
+import rain from "./images/rain.png";
+
 export default {
-  unnknown: {
-    symbol: "unnknown",
-    name: "不明"
+  unknown: {
+    symbol: unknown,
+    name: "不明",
+    subscript: ""
   },
   snowstorm: {
-    symbol: "snowstorm",
-    name: "地吹雪"
+    symbol: snowstorm,
+    name: "地吹雪",
+    subscript: ""
   },
   sandstorm: {
-    symbol: "sandstorm",
-    name: "砂塵嵐"
+    symbol: sandstorm,
+    name: "砂塵嵐",
+    subscript: ""
   },
   dusthaze: {
-    symbol: "dusthaze",
-    name: "塵煙霧"
+    symbol: dusthaze,
+    name: "塵煙霧",
+    subscript: ""
   },
   haze: {
-    symbol: "haze",
-    name: "煙霧"
+    symbol: haze,
+    name: "煙霧",
+    subscript: ""
   },
   hail: {
-    symbol: "hail",
-    name: "ひょう"
+    symbol: hail,
+    name: "ひょう",
+    subscript: ""
   },
   hailstone: {
-    symbol: "hailstone",
-    name: "あられ"
+    symbol: hailstone,
+    name: "あられ",
+    subscript: ""
   },
   snow: {
-    symbol: "snow",
-    name: "雪"
+    symbol: snow,
+    name: "雪",
+    subscript: ""
   },
-  "heavy-snow": {
-    symbol: "snow",
+  heavySnow: {
+    symbol: snow,
     name: "雪強し",
     subscript: "ツ"
   },
-  "snow-flurry": {
-    symbol: "snow",
+  snowFlurry: {
+    symbol: snow,
     name: "にわか雪",
     subscript: "ニ"
   },
   thunder: {
-    symbol: "thunder",
-    name: "雷"
+    symbol: thunder,
+    name: "雷",
+    subscript: ""
   },
-  "intense-thunder": {
-    symbol: "thunder",
+  intenseThunder: {
+    symbol: thunder,
     name: "雷強し",
     subscript: "ツ"
   },
   sleet: {
-    symbol: "sleet",
-    name: "みぞれ"
+    symbol: sleet,
+    name: "みぞれ",
+    subscript: ""
   },
   fog: {
-    symbol: "fog",
-    name: "霧"
+    symbol: fog,
+    name: "霧",
+    subscript: ""
   },
   cloudy: {
-    symbol: "cloudy",
-    name: "曇"
+    symbol: cloudy,
+    name: "曇",
+    subscript: ""
   },
   sunny: {
-    symbol: "sunny",
-    name: "晴"
+    symbol: sunny,
+    name: "晴",
+    subscript: ""
   },
   clear: {
-    symbol: "clear",
-    name: "快晴"
+    symbol: clear,
+    name: "快晴",
+    subscript: ""
   },
   rain: {
-    symbol: "rain",
-    name: "雨"
+    symbol: rain,
+    name: "雨",
+    subscript: ""
   },
-  "heavy-rain": {
-    symbol: "rain",
+  heavyRain: {
+    symbol: rain,
     name: "雨強し",
     subscript: "ツ"
   },
   shower: {
-    symbol: "rain",
+    symbol: rain,
     name: "にわか雨",
     subscript: "ニ"
   }
